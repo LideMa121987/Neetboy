6ed7d92a38b3efdd3b1f079383eef4a64243f532
Zhiwei.Ma <zhiwei.ma@ipinyou.com>
2014-05-06 16:41:53 +0800

    weibo sdk

---
+++ Neetboy/Neetboy/Controller/LoginViewController.h
@@ -0,0 +1,16 @@
+//
+//  LoginViewController.h
+//  Neetboy
+//
+//  Created by lide on 14-5-6.
+//  Copyright (c) 2014年 lide. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface LoginViewController : UIViewController
+{
+    UIButton    *_loginButton;
+}
+
+@end
